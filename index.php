0004224cf14be3c970f93ace8922ab735b8a634b
rossanaruvalcaba <rossana.unam@gmail.com>
2017-10-14 23:34:11 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php  
+require_once __DIR__.'/vendor/autoload.php';
+
+use GuzzleHttp\Client;
+use Symfony\Component\HttpFoundation\Response;
+
+//$api_key = getenv ('OPEN_WEATHER_API');
+$api_key = getenv ('OpenWea');
+
+$app = new Silex\Application();
+
+$app->get('/clima', function() use($app,$api_key) {
+                
+        $client = new client();
+        $url = "http://api.openweathermap.org/data/2.5/weather?id=546633&appid=405f8a53a56edd41d4c4842656145ec7&units=metric";
+        
+        $response = $client -> get ($url);
+        $body = $response -> getBody ();
+
+        return new Response ($body,200,array('Content-Type' => 'application/json'));
+});
+
+$app->run();
+
+?>
